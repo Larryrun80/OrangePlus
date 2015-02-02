47629ec0cebd19260bc7a5a3fe288b90a47e59d1
orange <ijoyce1104@gmail.com>
2015-02-02 17:02:36 +0800

    long time no see

--- expiringmerchants.py
+++ expiringmerchants.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# filename: 45days.py
+# 即将到期商户通知
 
 import sys
 import time
@@ -9,7 +9,8 @@ from orangejuice.utils.orangecsv import OrangeCsv
 from orangejuice.utils.orangemail import OrangeMail
 
 PERIOD = 45  # 查看距今n天后会到期的商户
-FILENAME = sys.path[0] + '/backup/expiringmerchants_' + time.strftime("%Y%m%d", time.localtime()) + '.xls'
+FILENAME = sys.path[0] + '/backup/expiringmerchants_' +time.strftime("%Y%m%d", time.localtime()) + '.xls'
+FILEHEADER = [('商户编号', '商户', '销售', '商品', '下线时间')]
 ENCODER = ['utf-8', 'gb18030']
 RECIPIENTS = ['zhujue@doweidu.com', 'guonan@doweidu.com']
 # now = int(time.time())
@@ -19,7 +20,11 @@ def get_expiring_merchants():
     # 取出所有即将到期的门店以及跟踪销售
     db_expiring_merchants_handler = OrangeMySQL('DB_ORANGE')
     querystring_shop_enddate = '''
-        select s.shopId,s.shopName,seller.name,g.goodsName,max(g.endDate) as endDate
+        select s.shopId,
+               s.shopName,
+               seller.name,
+               g.goodsName,
+               max(g.endDate) as endDate
         from murcielago_shop s,
                    shop_seller ss,
                    seller,
@@ -48,7 +53,7 @@ def get_expiring_merchants():
 def generate_file(data):
     csv_handler = OrangeCsv()
     csv_file = FILENAME
-    header = [('商户编号', '商户', '销售', '商品', '下线时间')]
+    header = FILEHEADER
     return csv_handler.write(csv_file, header + data, ENCODER)
 
 
--- oversold.py
+++ oversold.py
@@ -4,53 +4,54 @@
 from orangejuice.utils.orangemysql import OrangeMySQL
 # from orangejuice.utils.orangemail import OrangeMail
 
-db_stocksnumbers_hanlder = OrangeMySQL('DB_ORANGE')
+db_comparegswithsv_hanlder = OrangeMySQL('DB_ORANGE')
 
 
-# 取得当日在线商品的库存数
-def get_online_goods_data():
+def get_goodsstocks():
+    # 取得前日在线商品的库存数
     stocks = []
-    querstring_goods_stocks = '''
+    querstring_goods_data = '''
         select goodsId,
                goodsStocks
         from murcielago_goods
         where beginDate<=date_sub(CURDATE(),interval 1 day)
               and endDate>=date_sub(CURDATE(),interval 1 day)
               and enable='1'
+              order by goodsId
               limit 1000
         ;
     '''
-    result = db_stocksnumbers_hanlder.execute(querstring_goods_stocks)
+    result = db_comparegswithsv_hanlder.execute(querstring_goods_data)
     stocks = result.fetchall()
-    print(stocks)
+    # print(stocks)
     return stocks
 
 
-# 统计当日售出商品的数量
-def get_order_data():
-    numbers = []
-    querstring_sold_numers = '''
-        select og.goodsId ,count(o.orderId)
+def get_sealsvolume(stocks):
+    # 统计前日售出商品的数量
+    salesvolume = []
+    querstring_order_data = '''
+        select og.goodsId ,count(o.orderId) as volume
         from murcielago_order o ,murcielago_order_goods og
         where orderStatus in (1 ,2)
         and o.orderId=og.orderId
-        and DATE_FORMAT(from_unixtime(o.addTime),'%Y-%m-%d') =
+        and DATE_FORMAT(from_unixtime(o.addTime),'%Y-%m-%d')=
             date_sub(CURDATE() ,interval 1 day)
         group by og.goodsId
+        order by og.goodsId
         limit 1000
         ;
     '''
-    order = db_stocksnumbers_hanlder.execute(querstring_sold_numers)
-    numbers = order.fetchall()
-    print(numbers)
-    return numbers
+    order = db_comparegswithsv_hanlder.execute(querstring_order_data)
+    salesvolume = order.fetchall()
+    # print(salesvolume)
+    return salesvolume
 
-# 将两个数字进行比较
-
+    for (goodsId, stocks) in stocks:
+        if goodsId == og.goodsId:
+            stocks－volume
 
 # 如果售出数>库存数，发邮件通知
 
-get_online_goods_data()
-get_order_data()
 
-db_stocksnumbers_hanlder.close()
+db_comparegswithsv_hanlder.close()
